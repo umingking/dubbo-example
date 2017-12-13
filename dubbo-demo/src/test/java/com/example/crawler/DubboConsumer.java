02f1ed59cfb3556405d1c5320aba398bd2e3075f
umingking <umingking@aliyun.com>
2017-12-13 15:22:20 +0800

    修改忽略文件夹和消费者直连提供者

---
+++ dubbo-demo/src/test/java/com/example/crawler/DubboConsumer.java
@@ -0,0 +1,42 @@
+package com.example.crawler;
+
+import java.util.Arrays;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.MethodConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.example.crawler.demo.api.DemoApi;
+
+/**
+ * 
+ * @author umingking
+ * @desc 消费者直连指定ip的dubbo服务
+ *
+ */
+public class DubboConsumer {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        testDubboDemo();
+    }
+    
+    public static void testDubboDemo() {
+        ApplicationConfig ac = new ApplicationConfig();
+        ac.setName("crawler-demo-provider");
+        ReferenceConfig<DemoApi> ref = new ReferenceConfig<>();
+        ref.setInterface(DemoApi.class);
+        ref.setUrl("dubbo://127.0.0.1:20880/com.example.crawler.demo.api.DemoApi");
+        ref.setVersion("1.0.0");
+        ref.setApplication(ac);
+        MethodConfig mc = new MethodConfig();
+        mc.setAsync(false);
+        mc.setName("testDemo");
+        ref.setMethods(Arrays.asList(new MethodConfig[]{mc}));
+        
+        System.out.println(ref.get().testDemo("你好，dubbo框架"));
+    }
+
+}
+
